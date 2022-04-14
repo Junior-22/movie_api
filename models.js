e4511a65166b6330281c51532d1934bd59a5b9e1
Junior-22 <juniorfrancis2608@gmail.com>
2022-04-15 00:25:35 +0200

    created models.js

---
+++ models.js
@@ -0,0 +1,28 @@
+const mongooose = require("mongoose");
+
+let movieSchema = mongoose.Schema({
+  Title: { type: String, required: true },
+  Year: { type: String, required: true },
+  Description: { type: String, required: true },
+  Genre: { Name: String },
+  Director: {
+    Name: String,
+    Bio: String
+  },
+  Actors: [String],
+  Featured: Boolean
+});
+
+let userSchema = mongoose.Schema({
+  Username: { type: String, required: true },
+  Password: { type: String, required: true },
+  Email: { type: String, required: true },
+  Birthday: Date,
+  FavouriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }]
+});
+
+let Movie = mongoose.model("Movie", movieSchema);
+let User = mongoose.model("User", userSchema);
+
+module.exports.Movie = Movie;
+module.exports.User = User;
