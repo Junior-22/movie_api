49edb09c0f90bf7edc5c40f042cdf2649c878889
Junior-22 <juniorfrancis2608@gmail.com>
2022-05-17 12:38:16 +0200

    Update index.js
    add authentication logic to "/movies" endpoint

--- index.js
+++ index.js
@@ -55,16 +55,20 @@ app.get("/documentation", (req, res) => {
 });
 
 //Return a list of ALL movies to the user
-app.get("/movies", (req, res) => {
-  Movies.find()
-    .then(movies => {
-      res.status(200).json(movies);
-    })
-    .catch(err => {
-      console.error(err);
-      res.status(500).send("Error: " + err);
-    });
-});
+app.get(
+  "/movies",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Movies.find()
+      .then(movies => {
+        res.status(200).json(movies);
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      });
+  }
+);
 
 // Return movie by title
 app.get(
